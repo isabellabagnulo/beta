cd4fbeeabd5297a2db7543ee6f5f0756205fc8e8
isabellabagnulo <77104271+isabellabagnulo@users.noreply.github.com>
2022-04-08 11:54:11 +0200

    upload

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,32 @@
-(()=>{"use strict";document.addEventListener("DOMContentLoaded",(function(){var e=new ScrollMagic.Controller,l=(new TimelineMax).to("#js-slideContainer",1,{x:"-25%"}).to("#js-slideContainer",1,{x:"-50%"}).to("#js-slideContainer",1,{x:"-75%"});new ScrollMagic.Scene({triggerElement:"#js-wrapper",triggerHook:"onLeave",duration:"400%"}).setPin("#js-wrapper").setTween(l).addTo(e)}));const e=document.querySelector("#li-1"),l=document.querySelector("#li-2"),t=document.querySelector("#li-3");e.addEventListener("click",(()=>{e.style.flexGrow="1.5",l.style.flexGrow="0.2",t.style.flexGrow="0.2"})),l.addEventListener("click",(()=>{l.style.flexGrow="1.5",e.style.flexGrow="0.2",t.style.flexGrow="0.2"})),t.addEventListener("click",(()=>{t.style.flexGrow="1.5",l.style.flexGrow="0.2",e.style.flexGrow="0.2"})),e.addEventListener("mouseover",(()=>{e.style.flexGrow="1.5",l.style.flexGrow="0.2",t.style.flexGrow="0.2"})),l.addEventListener("mouseover",(()=>{l.style.flexGrow="1.5",e.style.flexGrow="0.2",t.style.flexGrow="0.2"})),t.addEventListener("mouseover",(()=>{t.style.flexGrow="1.5",l.style.flexGrow="0.2",e.style.flexGrow="0.2"})),e.addEventListener("mouseleave",(()=>{e.style.flexGrow="1",l.style.flexGrow="1",t.style.flexGrow="1"})),l.addEventListener("mouseleave",(()=>{e.style.flexGrow="1",l.style.flexGrow="1",t.style.flexGrow="1"})),t.addEventListener("mouseleave",(()=>{e.style.flexGrow="1",l.style.flexGrow="1",t.style.flexGrow="1"}))})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (() => {
+
+eval("const scrollContainer = document.querySelector(\"main\")\r\n\r\nscrollContainer.addEventListener(\"wheel\", (evt) => {\r\n  evt.preventDefault()\r\n  scrollContainer.scrollLeft += evt.deltaY\r\n})\r\n\r\nconst li1 = document.querySelector(\"#li-1\")\r\nconst li2 = document.querySelector(\"#li-2\")\r\nconst li3 = document.querySelector(\"#li-3\")\r\n/* click li */\r\nli1.addEventListener(\"click\", () => {\r\n  li1.style.flexGrow = \"1.5\"\r\n  li2.style.flexGrow = \"0.4\"\r\n  li3.style.flexGrow = \"0.4\"\r\n})\r\nli2.addEventListener(\"click\", () => {\r\n  li2.style.flexGrow = \"1.5\"\r\n  li1.style.flexGrow = \"0.4\"\r\n  li3.style.flexGrow = \"0.4\"\r\n})\r\nli3.addEventListener(\"click\", () => {\r\n  li3.style.flexGrow = \"1.5\"\r\n  li2.style.flexGrow = \"0.4\"\r\n  li1.style.flexGrow = \"0.4\"\r\n})\r\n// mouseover li\r\nli1.addEventListener(\"mouseover\", () => {\r\n  li1.style.flexGrow = \"1.5\"\r\n  li2.style.flexGrow = \"0.4\"\r\n  li3.style.flexGrow = \"0.4\"\r\n})\r\nli2.addEventListener(\"mouseover\", () => {\r\n  li2.style.flexGrow = \"1.5\"\r\n  li1.style.flexGrow = \"0.4\"\r\n  li3.style.flexGrow = \"0.4\"\r\n})\r\nli3.addEventListener(\"mouseover\", () => {\r\n  li3.style.flexGrow = \"1.5\"\r\n  li2.style.flexGrow = \"0.4\"\r\n  li1.style.flexGrow = \"0.4\"\r\n})\r\n/* mouseleave li*/\r\nli1.addEventListener(\"mouseleave\", () => {\r\n  li1.style.flexGrow = \"1\"\r\n  li2.style.flexGrow = \"1\"\r\n  li3.style.flexGrow = \"1\"\r\n})\r\nli2.addEventListener(\"mouseleave\", () => {\r\n  li1.style.flexGrow = \"1\"\r\n  li2.style.flexGrow = \"1\"\r\n  li3.style.flexGrow = \"1\"\r\n})\r\nli3.addEventListener(\"mouseleave\", () => {\r\n  li1.style.flexGrow = \"1\"\r\n  li2.style.flexGrow = \"1\"\r\n  li3.style.flexGrow = \"1\"\r\n})\r\n\n\n//# sourceURL=webpack://beta/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
-import { horizontalScroll } from "./scroll"
+const scrollContainer = document.querySelector("main")
 
-document.addEventListener("DOMContentLoaded", horizontalScroll)
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault()
+  scrollContainer.scrollLeft += evt.deltaY
+})
 
 const li1 = document.querySelector("#li-1")
 const li2 = document.querySelector("#li-2")
@@ -8,34 +11,34 @@ const li3 = document.querySelector("#li-3")
 /* click li */
 li1.addEventListener("click", () => {
   li1.style.flexGrow = "1.5"
-  li2.style.flexGrow = "0.2"
-  li3.style.flexGrow = "0.2"
+  li2.style.flexGrow = "0.4"
+  li3.style.flexGrow = "0.4"
 })
 li2.addEventListener("click", () => {
   li2.style.flexGrow = "1.5"
-  li1.style.flexGrow = "0.2"
-  li3.style.flexGrow = "0.2"
+  li1.style.flexGrow = "0.4"
+  li3.style.flexGrow = "0.4"
 })
 li3.addEventListener("click", () => {
   li3.style.flexGrow = "1.5"
-  li2.style.flexGrow = "0.2"
-  li1.style.flexGrow = "0.2"
+  li2.style.flexGrow = "0.4"
+  li1.style.flexGrow = "0.4"
 })
 // mouseover li
 li1.addEventListener("mouseover", () => {
   li1.style.flexGrow = "1.5"
-  li2.style.flexGrow = "0.2"
-  li3.style.flexGrow = "0.2"
+  li2.style.flexGrow = "0.4"
+  li3.style.flexGrow = "0.4"
 })
 li2.addEventListener("mouseover", () => {
   li2.style.flexGrow = "1.5"
-  li1.style.flexGrow = "0.2"
-  li3.style.flexGrow = "0.2"
+  li1.style.flexGrow = "0.4"
+  li3.style.flexGrow = "0.4"
 })
 li3.addEventListener("mouseover", () => {
   li3.style.flexGrow = "1.5"
-  li2.style.flexGrow = "0.2"
-  li1.style.flexGrow = "0.2"
+  li2.style.flexGrow = "0.4"
+  li1.style.flexGrow = "0.4"
 })
 /* mouseleave li*/
 li1.addEventListener("mouseleave", () => {
